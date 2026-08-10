8773c978d85ed1c5b82c022184bc3a8d13f6620a
santhu <santhoshnerella7@gmail.com>
2026-08-10 21:35:02 +0530

    Realtional operators practise

---
+++ RelationalOperators.java
@@ -0,0 +1,18 @@
+public class RelationalOperators {
+    public static void main(String[] args) {
+        int a = 90;
+        int b = 89;
+        int l = 34;
+        int m = 33;
+        System.out.println(b > a);
+        System.out.println(m < a);
+        boolean result = l < m;
+        System.out.println(result);
+        boolean ans = a == b ;
+        ans = false;
+        System.out.println(ans);
+
+        System.out.println(b <= a);
+    }
+    
+}
