cb766e810842fe85da5f80c82597c3e9cfecf144
santhu <santhoshnerella7@gmail.com>
2026-08-11 22:26:08 +0530

    Loopsing statements

---
+++ Looping.java
@@ -0,0 +1,46 @@
+// import java.util.*;
+public class Looping {
+// ----------------------------------------------------------------------------------------------------------->
+    // static void Eligibility(int a){
+    //     if(a >= 18 && a > 1){
+    //         System.out.println("Your are eligible to vote");
+    //     }else{
+    //         System.out.println("Your are not allowed to Vote");
+    //     }
+    // }
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.println("PLZZ Enter your Age");
+    //     int age = sc.nextInt();
+    //     Eligibility(age);
+    //     sc.close();
+    // }
+    //----------------------------------------------------------------------------------------------------------->
+    public static void main(String[] args) {
+
+        // int count = 1;
+        // while(true){
+        //     System.out.println("Tell me your Count" + count);
+        // }
+    //    while (count < 5) {
+    //     System.out.println("Tell me your Count: " + count);
+    //     count++;
+        
+    //    }
+
+    // do{
+    //     System.out.println("Your count is : " +  count);
+    // }while(count > 2);
+//------------------------------------------------------------------------------------------------------------------------------------->
+
+// for(int i = 1 ; i < 5 ; i++){
+//     System.out.println("*" + i);
+// }
+
+//---------------------------------------------------------------------------------------------------------------------------------------->
+      for(int i = 1 ; i <= 10 ; i++){
+        System.out.println("5 X " + i  +  "=" + " " + ( 5 * i));
+      }
+    }
+    
+}
