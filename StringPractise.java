5949b7a073f232e686fb2755d08623548b5e1733
santhu <santhoshnerella7@gmail.com>
2026-08-13 22:44:46 +0530

    String Usage and its methods

---
+++ StringPractise.java
@@ -0,0 +1,30 @@
+public class StringPractise {
+    public static void main(String[] args) {
+
+        // String name = new String("Santhosh");
+
+        // name = name + "Nerella";
+
+        // String n1 = "Chandu";
+        // String n2 = "chandu";
+        // System.out.println(n1 == n2);
+
+        // System.out.println(name);
+
+
+        // String name = "Santhosh";
+        // for(int i = 0 ; i <name.length(); i++){
+        //     System.out.print(name.charAt(i) + " ");
+        // }
+        //---------------------------------------------------------------------------------------->
+          String name = "Santhosh";
+          System.out.println(name.charAt(4));
+          System.out.println(name.length());
+          System.out.println(name.startsWith("San"));
+          System.out.println(name.endsWith("osh"));
+          System.out.println(name.toUpperCase());
+          System.out.println(name.toLowerCase());
+        
+    }
+    
+}
